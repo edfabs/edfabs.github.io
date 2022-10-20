315fd26517d9ca6560cd4dfad55c97369c1eba9f
Fabian Suchett <suchettr@gmail.com>
2022-10-19 19:42:57 -0500

    Add function to load images

--- LigaPetrolera/petro.js
+++ LigaPetrolera/petro.js
@@ -2,15 +2,15 @@ var grid = document.querySelector('.grid');
 var posts = document.querySelectorAll('.grid-item');
 imagesLoaded( posts, function() {
     console.log("images loaded");
-});
-var msnry = new Masonry( grid, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
-
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-  // options
+    var msnry = new Masonry( grid, {
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: 200
+      });
+      
+      // element argument can be a selector string
+      //   for an individual element
+      var msnry = new Masonry( '.grid', {
+        // options
+      });
 });
